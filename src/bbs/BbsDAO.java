e6f2ffe7fdfa997d873ef66a86ced658aebbaec3
gggjhu2 <gggjhu2@gmail.com>
2020-07-06 18:20:29 +0900

    브런치따기 연습! 브런치이름은 1-01

--- src/bbs/BbsDAO.java
+++ src/bbs/BbsDAO.java
@@ -133,7 +133,22 @@ public class BbsDAO {
 		}
 			return null;
 		}
+		public int update(int bbsID,String bbsTitle,String bbsContent){
+			
+			String SQL = "UPDATE BBS SET bbsTitle = ?, bbsContent = ? WHERE bbsID = ? ";
+			try{
+				 PreparedStatement pstmt=conn.prepareStatement(SQL);
+				 pstmt.setString(1, bbsTitle);
+				 pstmt.setString(2, bbsContent);
+				 pstmt.setInt(3, bbsID);
+				 return pstmt.executeUpdate();
+		}catch(Exception e){
+			e.printStackTrace();
 		}
+			return -1;//데이터베이스오류	
+		}
+}
+
 
 
 
